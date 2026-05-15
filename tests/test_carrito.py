52b0689781d5439376adea12ef8cd55deee067ae
IsabellaAriasCue <iarias579@cue.edu.co>
2026-05-14 19:07:52 -0500

    RF7 - Red

--- tests/test_carrito.py
+++ tests/test_carrito.py
@@ -9,3 +9,14 @@ def test_aplicar_cupon_porcentaje():
     carrito.aplicar_cupon(tipo="porcentaje", valor=15)
 
     assert carrito.calcular_total() == 85000
+
+def test_vaciar_carrito():
+    carrito = Carrito()
+
+    carrito.agregar_producto(Producto("Laptop", 100000, 1))
+    carrito.agregar_producto(Producto("Mouse", 50000, 1))
+
+    carrito.vaciar()
+
+    assert len(carrito.productos) == 0
+    assert carrito.calcular_total() == 0
