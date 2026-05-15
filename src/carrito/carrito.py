103353cdf75e3efab01c6b7ab1e9a289c7b8b2da
IsabellaAriasCue <iarias579@cue.edu.co>
2026-05-14 19:02:56 -0500

    RF4 - Refactor

--- src/carrito/carrito.py
+++ src/carrito/carrito.py
@@ -4,18 +4,23 @@ from .modelos import Producto
 class Carrito:
     def __init__(self):
         self.productos = []
-        self.descuento = 0
+        self.tipo_cupon = None
+        self.valor_cupon = 0
 
     def agregar_producto(self, producto: Producto):
         self.productos.append(producto)
 
     def aplicar_cupon(self, tipo, valor):
-        if tipo == "porcentaje":
-            self.descuento = valor / 100
+        self.tipo_cupon = tipo
+        self.valor_cupon = valor
 
     def calcular_total(self):
         subtotal = sum(
             producto.precio * producto.cantidad
             for producto in self.productos
         )
-        return subtotal * (1 - self.descuento)
+
+        if self.tipo_cupon == "porcentaje":
+            return subtotal * (1 - self.valor_cupon / 100)
+
+        return subtotal
