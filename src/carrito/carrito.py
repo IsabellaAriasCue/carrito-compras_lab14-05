9371d95301d9deca7a16ff44faf862e423f86b8b
IsabellaAriasCue <iarias579@cue.edu.co>
2026-05-14 19:00:47 -0500

    RF4 - Green

--- src/carrito/carrito.py
+++ src/carrito/carrito.py
@@ -4,9 +4,18 @@ from .modelos import Producto
 class Carrito:
     def __init__(self):
         self.productos = []
+        self.descuento = 0
 
     def agregar_producto(self, producto: Producto):
         self.productos.append(producto)
 
+    def aplicar_cupon(self, tipo, valor):
+        if tipo == "porcentaje":
+            self.descuento = valor / 100
+
     def calcular_total(self):
-        return sum(producto.precio * producto.cantidad for producto in self.productos)
+        subtotal = sum(
+            producto.precio * producto.cantidad
+            for producto in self.productos
+        )
+        return subtotal * (1 - self.descuento)
